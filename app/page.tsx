1835e6edc965d96b90a1da1cf1981869cf4c86d5
he.wenyao <he.wenyao@nint.com>
2024-07-26 11:37:14 +0800

    初始化项目

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Image
+        src="/6168935.png"
+        alt="ibed Logo"
+        width={72}
+        height={16}></Image>
+    </div>
+  );
+}
